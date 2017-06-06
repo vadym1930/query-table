e5c952b3c708885883338484707e5dae991f0a46
Vadym Horbonos <horbonosvadym@gmail.com>
2017-06-07 01:26:02 +0300

    make fetch method

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,14 @@
 console.log('here !!');
 require('../scss/style.scss');
 
-const sum = (a, b) => a + b;
+fetch(`https://api.themoviedb.org/3/discover/movie?api_key=7e9aa3015d3ebeaa49578d245d8cdf1b`)
+    .then((response)=>{
+        console.log(response.status);
+        if(response.status != 200){
+            return
+        }
+        response.json().then(data => {
+            console.log(data);
 
-console.log(sum(3,5));
+        });
+    });
