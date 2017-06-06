3cd73d649f0e7e301845dd90ec18f9b4699034fe
Vadym Horbonos <horbonosvadym@gmail.com>
2017-06-06 23:28:35 +0300

    configure dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 5000
+    },
     module: {
         rules: [
             {
