b35dd2dff5ec1ba400bb08774c59b13a3adb0a71
Vadym Horbonos <horbonosvadym@gmail.com>
2017-06-26 01:12:45 +0300

    remove pagination animation

--- src/scripts/gsap.module.js
+++ src/scripts/gsap.module.js
@@ -7,17 +7,14 @@ const tl = new TimelineLite({ paused: true, onComplete });
 
 function onComplete() {
   const elPreloader = document.querySelector('.loader-covering');
-  const elBack = document.querySelector('.back-of-loader');
-  const elSearch = document.querySelector('#search');
-  const elPages = document.querySelectorAll('.page a');
-  console.log(elPages);
-  const tlLoader = new TimelineLite();
+  const elBack      = document.querySelector('.back-of-loader');
+  const elSearch    = document.querySelector('#search');
+  const tlLoader    = new TimelineLite();
 
   tlLoader
     .to(elBack, 1, { autoAlpha: 0 })
     .to(elPreloader, 1, { y: '-100%' }, 0)
-    .to(elSearch, 1, { width: '100%', autoAlpha: 1 }, 1)
-    .staggerFromTo(elPages, .3, { x: 100, autoAlpha: 0 }, { x: 0, autoAlpha: 1, marginRight: '15px' }, 0);
+    .to(elSearch, 1, { width: '100%', autoAlpha: 1 }, 1);
 }
 
 function setTl() {
@@ -27,7 +24,7 @@ function setTl() {
   tl
     .set(elMain, { autoAlpha: 1 })
     // .staggerFromTo(elTr, .2, { x: -50, autoAlpha: 0 }, { x: 0, autoAlpha: 1 }, .1);
-    .staggerFromTo(elTr, .2, { scale: -1, autoAlpha: 0 }, { scale: 1, autoAlpha: 1 }, .1);
+    .staggerFromTo(elTr, .2, { skewX: 90, autoAlpha: 0 }, { skewX: 0, autoAlpha: 1 }, .1);
 }
 
 function playTl() {
